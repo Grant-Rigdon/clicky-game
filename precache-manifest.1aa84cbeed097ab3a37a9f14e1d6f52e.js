0297cc60e5ae63f2f742257dbfd38efe3ca3784e
Grant Rigdon <grant_rigdon@ymail.com>
2019-01-16 21:04:04 -0500

    Updates

---
+++ precache-manifest.1aa84cbeed097ab3a37a9f14e1d6f52e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "a83631b44ab7977e1f07",
+    "url": "/clicky-game/static/js/main.a83631b4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a83631b44ab7977e1f07",
+    "url": "/clicky-game/static/css/main.884b9e3f.chunk.css"
+  },
+  {
+    "revision": "2a75247a077ebd144275e9fcbb281b68",
+    "url": "/clicky-game/index.html"
+  }
+];
